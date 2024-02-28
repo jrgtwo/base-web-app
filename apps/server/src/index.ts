607d46dee2fb15da2799a68291050533b4d95e07
Jonathan Garcia <jonathan.ray.garcia@gmail.com>
2024-02-28 08:04:09 -0800

    feat: 404 and 500 handlers

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { RequestHandler, ErrorRequestHandler, NextFunction, Request, Response } from 'express';
 import 'dotenv/config';
 
 const app = express();
@@ -6,6 +6,18 @@ const port = process.env.PORT || 8080;
 const host = process.env.HOST || 'localhost';
 
 app.use(express.static('../webapp/dist'));
+
+const catchAllHandler: RequestHandler = (_, res) => {
+  res.status(404).send('its a 404');
+};
+
+const errorHandler: ErrorRequestHandler = (err: Error, _req: Request, res: Response, _next: NextFunction) => {
+  console.log(err.stack);
+  res.status(500).send('its a 500!! Yikes');
+};
+
+app.use(catchAllHandler);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server running at  http://${host}:${port}`);
